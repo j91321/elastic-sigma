53c0f6488dcbe9a3f678a2d1f29d7176aa8f6813
j91321 <jan.trencansky@gmail.com>
2020-03-27 23:39:58 +0100

    Kibana Detection engine CI import test

--- test_rules_import.py
+++ test_rules_import.py
@@ -5,15 +5,12 @@ import argparse
 import json
 import os
 
-parser = argparse.ArgumentParser(description='Load kibana-saved-searches json and bulk insert them into Kibana')
-parser.add_argument('--url', type=str)
-parser.add_argument('--user', type=str)
-parser.add_argument('--password', type=str)
+parser = argparse.ArgumentParser(description='Import SIEM detection rules into Kibana')
+parser.add_argument('--kibana', type=str)
+parser.add_argument('--elasticsearch', type=str)
 args = parser.parse_args()
 
-url = base64.b64decode(args.url).decode('ascii')+'/api/detection_engine/rules/_import?overwrite=true'
-username = base64.b64decode(args.user).decode('ascii')
-password = base64.b64decode(args.password).decode('ascii')
+siem_url = args.kibana+'/api/detection_engine/rules/_import?overwrite=true'
 headers = {
   'kbn-xsrf': 'kibana'
   }
@@ -23,13 +20,21 @@ directory = os.fsencode(artifacts_path)
 
 exit_code = 0
 
+# This index (alias to be precise) must exist in order for Kibana to actually do the import, normally it is created by Kibana but that requires X-Pack Security to be configured, this hack will trick Kibana into doing the import
+dummy_index_url = args.elasticsearch+'/.siem-signals-default'
+print(dummy_index_url)
+response = requests.put(dummy_index_url)
+if not response.ok:
+    print("Error: Failed to create dummy .siem-signals-default index")
+    sys.exit(1)
+
 for sigma_rule in os.listdir(directory):
     filename = os.fsdecode(sigma_rule)
     if filename.endswith(".ndjson"):
         with open(os.path.join(artifacts_path, filename), 'rb') as ndjson_file:
             files = {'file': (filename, ndjson_file, 'application/octet-stream')}
             print("Importing " + filename, end=": ")
-            response = requests.post(url, headers=headers, auth=(username, password), files=files)
+            response = requests.post(siem_url, headers=headers, files=files)
             if response.ok:
                 response_json = json.loads(response.text)
                 if response_json['success']:
