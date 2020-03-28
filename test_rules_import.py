7409c060c110539e7ad4fc330ea8602bbedca334
j91321 <jan.trencansky@gmail.com>
2020-03-28 10:59:47 +0100

    Add changes that wait for Kibana API

--- test_rules_import.py
+++ test_rules_import.py
@@ -4,12 +4,14 @@ import requests
 import argparse
 import json
 import os
+import time
 
 parser = argparse.ArgumentParser(description='Import SIEM detection rules into Kibana')
 parser.add_argument('--kibana', type=str)
 parser.add_argument('--elasticsearch', type=str)
 args = parser.parse_args()
 
+kibana_api_test = args.kibana+'/api/features'
 siem_url = args.kibana+'/api/detection_engine/rules/_import?overwrite=true'
 headers = {
   'kbn-xsrf': 'kibana'
@@ -22,11 +24,22 @@ exit_code = 0
 
 # This index (alias to be precise) must exist in order for Kibana to actually do the import, normally it is created by Kibana but that requires X-Pack Security to be configured, this hack will trick Kibana into doing the import
 dummy_index_url = args.elasticsearch+'/.siem-signals-default'
-print(dummy_index_url)
 response = requests.put(dummy_index_url)
+print("Creating dummy .siem-signals-default")
 if not response.ok:
     print("Error: Failed to create dummy .siem-signals-default index")
     sys.exit(1)
+
+# Wait for Kibana service to start properly
+for i in range(10):
+    print("Waiting for Kibana API to become available")
+    response = requests.get(kibana_api_test, headers=headers)
+    if response.status_code == 503:
+        print(response.text)
+        time.sleep(10)
+    else:
+        print("Kibana API is ready")
+        break
 
 for sigma_rule in os.listdir(directory):
     filename = os.fsdecode(sigma_rule)
