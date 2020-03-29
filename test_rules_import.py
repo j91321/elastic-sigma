e83f2d9cc07dce277a11f7e57ed56815d0c79524
j91321 <jan.trencansky@gmail.com>
2020-03-29 17:23:57 +0200

    Revert CI to original, add yamllint

--- test_rules_import.py
+++ test_rules_import.py
@@ -4,15 +4,16 @@ import requests
 import argparse
 import json
 import os
-import time
 
-parser = argparse.ArgumentParser(description='Import SIEM detection rules into Kibana')
-parser.add_argument('--kibana', type=str)
-parser.add_argument('--elasticsearch', type=str)
+parser = argparse.ArgumentParser(description='Load kibana-saved-searches json and bulk insert them into Kibana')
+parser.add_argument('--url', type=str)
+parser.add_argument('--user', type=str)
+parser.add_argument('--password', type=str)
 args = parser.parse_args()
 
-kibana_api_test = args.kibana+'/api/features'
-siem_url = args.kibana+'/api/detection_engine/rules/_import?overwrite=true'
+url = base64.b64decode(args.url).decode('ascii')+'/api/detection_engine/rules/_import?overwrite=true'
+username = base64.b64decode(args.user).decode('ascii')
+password = base64.b64decode(args.password).decode('ascii')
 headers = {
   'kbn-xsrf': 'kibana'
   }
@@ -22,32 +23,13 @@ directory = os.fsencode(artifacts_path)
 
 exit_code = 0
 
-# This index (alias to be precise) must exist in order for Kibana to actually do the import, normally it is created by Kibana but that requires X-Pack Security to be configured, this hack will trick Kibana into doing the import
-dummy_index_url = args.elasticsearch+'/.siem-signals-default'
-response = requests.put(dummy_index_url)
-print("Creating dummy .siem-signals-default")
-if not response.ok:
-    print("Error: Failed to create dummy .siem-signals-default index")
-    sys.exit(1)
-
-# Wait for Kibana service to start properly
-for i in range(100):
-    print("Waiting for Kibana API to become available")
-    response = requests.get(kibana_api_test, headers=headers)
-    if response.status_code == 503:
-        print(response.text)
-        time.sleep(30)
-    else:
-        print("Kibana API is ready")
-        break
-
 for sigma_rule in os.listdir(directory):
     filename = os.fsdecode(sigma_rule)
     if filename.endswith(".ndjson"):
         with open(os.path.join(artifacts_path, filename), 'rb') as ndjson_file:
             files = {'file': (filename, ndjson_file, 'application/octet-stream')}
             print("Importing " + filename, end=": ")
-            response = requests.post(siem_url, headers=headers, files=files)
+            response = requests.post(url, headers=headers, auth=(username, password), files=files)
             if response.ok:
                 response_json = json.loads(response.text)
                 if response_json['success']:
@@ -56,7 +38,4 @@ for sigma_rule in os.listdir(directory):
                     print("Failed")
                     print(response.text)
                     exit_code = 1
-            else:
-                exit_code = 1
-                print(response.text)
 sys.exit(exit_code)
